3a93645e9565f9b2e16cf9a0eef10e38d1e1cdd1
MakeToLearn <maketolearn@Jordan-Reiters-MacBook-Pro.local>
2011-05-19 14:48:06 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-shapesapi',
+      version='1.0',
+      description='MakeToLearn Shapes API',
+      author='Jordan Reiter',
+      author_email='jordanreiter@gmail.com',
+      packages=['shapesapi'],
+     )
